274a2959e13e21c17790796151889998805ab5e1
ashdr <1037652584@qq.com>
2022-11-22 10:03:06 +0800

    update gitignore

---
+++ Codeforces/cf333/c.cpp
@@ -0,0 +1,143 @@
+/*
+           ▃▆█▇▄▖
+       ▟◤▖　　　  ◥█  
+   ◢◤ 　  ◢▐　　     ▐▉
+ ▗◤　  　   ▂ ▗▖　 ▕ █▎
+ ◤　▗▅▖ ◥▄　 ▀▀▀◣　█▊
+▐　▕▎  ◥▖◣◤　 　　◢██
+█◣　◥▅█▀　     　▐███◤
+▐█▙▂　　　      ◢███◤
+　◥██◣　　　　 ◢▄◤
+　　　▀██▅▇▀▎▇
+
+*/
+#include <bits/stdc++.h>
+#define LOG(FMT...) fprintf(stderr, FMT)
+#define sz(x) (int)x.size()
+using namespace std;
+typedef long long LL;
+typedef pair<int,int> pii;
+typedef pair<LL,LL> pll;
+template <class T>
+istream& operator>>(istream& is, vector<T>& v) {
+  for (T& x : v)
+    is >> x;
+  return is;
+}
+template <class T>
+ostream& operator<<(ostream& os, const vector<T>& v) {
+  if (!v.empty()) {
+    os << v.front();
+    for (int i = 1; i < (int)v.size(); ++i)
+    os << ' ' << v[i];
+  }
+  return os;
+}
+void dbg_out() { cerr << "\b\b )" << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T){cerr << H << ", ";dbg_out(T...);}
+#define debug(...) cerr << "( " << #__VA_ARGS__ << " ) = ( ", dbg_out(__VA_ARGS__)
+mt19937 myrand(chrono::steady_clock::now().time_since_epoch().count());
+LL gcd(LL x, LL y) {return y == 0 ? x : gcd(y, x % y);}
+LL qpow(LL base, LL x, LL mod) {
+    LL res = 1;
+    base %= mod;
+    while(x) {
+       if(x & 1) res = (res * base) % mod;
+       base = (base * base) % mod;
+       x >>= 1;
+    } 
+    return res;
+}
+LL exgcd(LL a,LL b,LL &x,LL &y) {
+    if(b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    LL d = exgcd(b,a % b,y,x);
+    y -= a / b * x;
+    return d;
+}// (get inv) gcd(a,p) = 1 
+
+const int N = 4e2 + 10;
+const int M = 1e5 + 10;
+const int INF = 2147483647;
+const LL MOD = 1e9 + 7;
+int TT = 1;
+int block[N][N];
+void solve() {
+    int n, m;
+    cin >> n >> m;
+    vector<vector<int>> G(n + 1,vector<int>()); 
+    for(int i = 0; i < m; i++) {
+        int u, v;
+        cin >> u >> v;
+        G[u].push_back(v);
+        G[v].push_back(u);
+        block[u][v] = block[v][u] = 1;
+    }
+    vector<bool> vis(n + 1,0);
+    int res1 = 0, res2 = 0;
+    auto bfs = [&]() {
+        queue<pii> q;
+        q.push({1,0});
+        vis[1] = 1;
+        while(!q.empty()) {
+            int u = q.front().first;
+            int d = q.front().second;
+            q.pop();
+            if(u == n) {
+                res1 = d;
+                break;
+            }
+            for(auto v: G[u]) {
+                if(!vis[v]) {
+                    vis[v] = 1;
+                    q.push({v,d + 1});
+                }
+            }
+        }       
+    };
+    bfs();
+    vector<bool> vis1(n + 1,0);
+    auto bfs1 = [&]() {
+        queue<pii> q;
+        q.push({1,0});
+        vis1[1] = 1;
+        while(!q.empty()) {
+            int u = q.front().first;
+            int d = q.front().second;
+            q.pop();
+            if(u == n) {
+                res2 = d;
+                break;
+            }
+            for(int i = 1; i <= n; i++) {
+                if(!vis1[i] && !block[u][i]) {
+                    vis1[i] = 1;
+                    q.push({i,d + 1});
+                }
+            }
+        }       
+    };
+    bfs1();
+    if(min(res1,res2) == 0) cout << -1 << '\n';
+    else cout << max(res1, res2) << '\n';
+}
+int main() {
+    #ifdef ASHDR
+    freopen("data.in","r",stdin);
+    int nol_cl = clock();
+    #endif
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout<<fixed<<setprecision(8);
+    //cin>>TT;
+    while(TT--) solve();
+    #ifdef ASHDR
+    LOG("Time: %dms\n", int ((clock()
+            -nol_cl) / (double)CLOCKS_PER_SEC * 1000));
+    #endif
+    return 0;
+}
