f9f7ba156c5f5a3138660c9568f80f89b0a117d4
ashdr <1037652584@qq.com>
2023-10-21 22:44:36 +0800

    add st template

---
+++ Atcoder/KEYENCE Programming Contest 2023 Autumn（AtCoder Beginner Contest 325）/C.cpp
@@ -0,0 +1,145 @@
+/*
+           ▃▆█▇▄▖
+       ▟◤▖　　　  ◥█  
+   ◢◤ 　  ◢▐　　     ▐▉
+ ▗◤　  　   ▂ ▗▖　 ▕ █▎
+ ◤　▗▅▖ ◥▄　 ▀▀▀◣　█▊
+▐　▕▎  ◥▖◣◤　 　　◢██
+█◣　◥▅█▀　     　▐███◤
+▐█▙▂　　　      ◢███◤
+　◥██◣　　　　 ◢▄◤
+　　　▀██▅▇▀▎▇
+
+*/
+#include <bits/stdc++.h>
+#include <random>
+#define LOG(FMT...) fprintf(stderr, FMT)
+#define sz(x) (int)x.size()
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+typedef pair<double,double> pdd;
+template <class T>
+istream& operator>>(istream& is, vector<T>& v) {
+  for (T& x : v)
+    is >> x;
+  return is;
+}
+template <class T>
+ostream& operator<<(ostream& os, const vector<T>& v) {
+  if (!v.empty()) {
+    os << v.front();
+    for (int i = 1; i < (int)v.size(); ++i)
+    os << ' ' << v[i];
+  }
+  return os;
+}
+void dbg_out() { cerr << "\b\b )" << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T){cerr << H << ", ";dbg_out(T...);}
+#define debug(...) cerr << "( " << #__VA_ARGS__ << " ) = ( ", dbg_out(__VA_ARGS__)
+mt19937_64 myrand(chrono::steady_clock::now().time_since_epoch().count());
+ll myRand(ll B) {return (ull)myrand()%B;}
+ll gcd(ll x, ll y) {return y == 0 ? x : gcd(y, x % y);}
+ll qpow(ll base, ll x, ll mod) {
+    ll res = 1;
+    base %= mod;
+    while(x) {
+       if(x & 1) res = (res * base) % mod;
+       base = (base * base) % mod;
+       x >>= 1;
+    } 
+    return res;
+}
+ll exgcd(ll a,ll b,ll &x,ll &y) {
+    if(b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    ll d = exgcd(b,a % b,y,x);
+    y -= a / b * x;
+    return d;
+}// (get inv) gcd(a,p) = 1 
+
+const int N = 1e3 + 10;
+const int M = 1e3 + 10;
+const int INF = 2147483647;
+const ll MOD = 1e9 + 7;
+int TT = 1;
+struct DSU {
+    std::vector<int> f, siz;
+    DSU(int n) : f(n), siz(n, 1) { std::iota(f.begin(), f.end(), 0); }
+    int leader(int x) {
+        while (x != f[x]) x = f[x] = f[f[x]];
+        return x;
+    }
+    bool same(int x, int y) { return leader(x) == leader(y); }
+    bool merge(int x, int y) {
+        x = leader(x);
+        y = leader(y);
+        if (x == y) return false;
+        siz[x] += siz[y];
+        f[y] = x;
+        return true;
+    }
+    int size(int x) { return siz[leader(x)]; }
+};
+char s[N][N];
+const int dx[] = {-1,1,0,0,1,1,-1,-1};
+const int dy[] = {0,0,1,-1,1,-1,1,-1};
+void solve() {
+  	int n, m;
+  	cin >> n >> m;
+  	for(int i = 1; i <= n; i++) {
+  		cin >> (s[i] + 1);
+  	}
+  	DSU dsu(n * m + 1);
+  	auto check = [&](int x, int y) {
+  		if(x < 1 || x > n || y < 1 || y > m) return false;
+  		return true;
+  	};
+  	auto getid = [&](int x, int y) {
+  		return (x - 1) * m + y;
+  	};
+  	for(int i = 1; i <= n; i++) {
+  		for(int j = 1; j <= m; j++) {
+			if(s[i][j] != '#') continue;
+  			for(int k = 0; k < 8; k++) {
+  				int x = i + dx[k],y = j + dy[k];
+  				if(check(x, y) && s[x][y] == '#') {
+  					dsu.merge(getid(i, j), getid(x, y));
+  				}
+  			}
+  		}
+  	}
+  	int ans = 0;
+  	for(int i = 1; i <= n; i++) {
+  		for(int j = 1; j <= m; j++) {
+  			if(getid(i, j) == dsu.leader(getid(i, j)) && s[i][j] == '#') {
+  				++ans;
+  			}
+  		}
+  	}
+  	cout << ans << "\n";
+}
+int main() {
+    #ifdef ASHDR
+    freopen("data.in","r",stdin);
+    freopen("data.out","w",stdout);
+    int nol_cl = clock();
+    #endif
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout<<fixed<<setprecision(8);
+    //cin>>TT;
+    while(TT--) solve();
+    #ifdef ASHDR
+    LOG("Time: %dms\n", int ((clock()
+            -nol_cl) / (double)CLOCKS_PER_SEC * 1000));
+    #endif
+    return 0;
+}
---
+++ Atcoder/KEYENCE Programming Contest 2023 Autumn（AtCoder Beginner Contest 325）/E.cpp
@@ -0,0 +1,126 @@
+/*
+           ▃▆█▇▄▖
+       ▟◤▖　　　  ◥█  
+   ◢◤ 　  ◢▐　　     ▐▉
+ ▗◤　  　   ▂ ▗▖　 ▕ █▎
+ ◤　▗▅▖ ◥▄　 ▀▀▀◣　█▊
+▐　▕▎  ◥▖◣◤　 　　◢██
+█◣　◥▅█▀　     　▐███◤
+▐█▙▂　　　      ◢███◤
+　◥██◣　　　　 ◢▄◤
+　　　▀██▅▇▀▎▇
+
+*/
+#include <bits/stdc++.h>
+#include <functional>
+#include <queue>
+#include <random>
+#define LOG(FMT...) fprintf(stderr, FMT)
+#define sz(x) (int)x.size()
+using namespace std;
+typedef long long ll;
+typedef pair<int,int> pii;
+typedef pair<ll,ll> pll;
+typedef unsigned int ui;
+typedef unsigned long long ull;
+typedef pair<double,double> pdd;
+template <class T>
+istream& operator>>(istream& is, vector<T>& v) {
+  for (T& x : v)
+    is >> x;
+  return is;
+}
+template <class T>
+ostream& operator<<(ostream& os, const vector<T>& v) {
+  if (!v.empty()) {
+    os << v.front();
+    for (int i = 1; i < (int)v.size(); ++i)
+    os << ' ' << v[i];
+  }
+  return os;
+}
+void dbg_out() { cerr << "\b\b )" << endl; }
+template <typename Head, typename... Tail>
+void dbg_out(Head H, Tail... T){cerr << H << ", ";dbg_out(T...);}
+#define debug(...) cerr << "( " << #__VA_ARGS__ << " ) = ( ", dbg_out(__VA_ARGS__)
+mt19937_64 myrand(chrono::steady_clock::now().time_since_epoch().count());
+ll myRand(ll B) {return (ull)myrand()%B;}
+ll gcd(ll x, ll y) {return y == 0 ? x : gcd(y, x % y);}
+ll qpow(ll base, ll x, ll mod) {
+    ll res = 1;
+    base %= mod;
+    while(x) {
+       if(x & 1) res = (res * base) % mod;
+       base = (base * base) % mod;
+       x >>= 1;
+    } 
+    return res;
+}
+ll exgcd(ll a,ll b,ll &x,ll &y) {
+    if(b == 0) {
+        x = 1;
+        y = 0;
+        return a;
+    }
+    ll d = exgcd(b,a % b,y,x);
+    y -= a / b * x;
+    return d;
+}// (get inv) gcd(a,p) = 1 
+
+const int N = 1e3 + 10;
+const int M = 1e5 + 10;
+const int INF = 2147483647;
+const ll MOD = 1e9 + 7;
+int TT = 1;
+int n;
+ll d[N][N];
+ll a,b,c;
+void solve() {
+    cin >> n >> a >> b >> c;
+    vector<vector<pair<int,ll>>> G(2 * n + 1);
+    vector<ll> dis(2 * n + 1,1e18);
+    vector<int> vis(2 * n + 1);
+    for(int i = 1; i <= n; i++) {
+    	for(int j = 1; j <= n; j++) {
+    		cin >> d[i][j];
+    		if(i != j) {
+    			G[i].push_back({j, d[i][j] * a});
+    			G[i].push_back({i + n, 0});
+    			G[i + n].push_back({j + n, d[i][j] * b + c});
+    		}
+    	}
+    }
+    priority_queue<pair<ll,int>,vector<pair<ll,int>>,greater<pair<ll,int> >> heap;
+    heap.push({0, 1});
+    dis[1] = 0;
+    while(!heap.empty()) {
+    	auto [dd, u] = heap.top();heap.pop();
+    	if(vis[u]) continue;
+    	vis[u] = 1;
+    	for(auto [v, w]: G[u]) {
+    		if(dis[v] > dis[u] + w) {
+    			dis[v] = dis[u] + w;
+    			heap.push({dis[v], v});
+    		}
+    	}
+    }
+    cout << dis[2 * n] << "\n";
+    
+}
+int main() {
+    #ifdef ASHDR
+    freopen("data.in","r",stdin);
+    freopen("data.out","w",stdout);
+    int nol_cl = clock();
+    #endif
+    ios::sync_with_stdio(0);
+    cin.tie(nullptr);
+    cout<<fixed<<setprecision(8);
+    //cin>>TT;
+    while(TT--) solve();
+    #ifdef ASHDR
+    LOG("Time: %dms\n", int ((clock()
+            -nol_cl) / (double)CLOCKS_PER_SEC * 1000));
+    #endif
+    return 0;
+}
